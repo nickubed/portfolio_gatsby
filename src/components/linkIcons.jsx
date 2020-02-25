0442a0bf228d9e04e8b3a3e3d0d46a3eb5845f0e
nickubed <nhpquandt@gmail.com>
2020-02-25 11:57:18 -0800

    more styling, links work

--- src/components/linkIcons.jsx
+++ src/components/linkIcons.jsx
@@ -5,8 +5,9 @@ import linkedIn from '../images/linkedIn_logo.png'
 const Links = () => {
     return (
         <div id="linkPack">
-            <img src={github} alt="github logo" id="github_logo" />
-            <img src={linkedIn} alt="linkedin logo" id="linkedin_logo" />
+            <a href="https://github.com/nickubed"><img src={github} alt="github logo" id="github_logo" /></a>
+            <a href="https://www.linkedin.com/in/nick-quandt/"><img src={linkedIn} alt="linkedin logo" id="linkedin_logo" /></a>
+            <p id="resume">resume</p>
         </div>
     )
 }
