ac0e1bbbb17caa3c0819354fe700eabf37c0901d
nickubed <nhpquandt@gmail.com>
2020-02-26 18:17:16 -0800

    Further styling, added some content for serfslist

--- src/components/project_3.jsx
+++ src/components/project_3.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SocialImage from './social-life'
+import SerfsImg from './serfs-list'
 
 
 const Project3 = () => {
@@ -9,7 +9,6 @@ const Project3 = () => {
                 <div className="col-0 col-md-6"></div>
                 <div className="col-12 col-md-6">
                     <h2 className="subhead"></h2>
-                    
                 </div>
             </div>
             <div className="row row-cols-2">
@@ -18,15 +17,19 @@ const Project3 = () => {
                 </div>
                 <div className="col-12 col-md-5">
                     <br/>
-                    <div id="about"><p className="smallTitle">busker street</p>
+                    <div id="about"><p className="smallTitle">hackathon piece: serfslist</p>
                     <hr/>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque veniam earum maxime eaque? Voluptate perspiciatis saepe obcaecati nobis voluptatum impedit eos quam eligendi ad doloribus! Quos unde neque nihil libero.
+                    <p>serfslist was created over 24 hours with a team of two other developers and a UX designer.
+                        the prompt provided was "medieval life", and the date of delivery would be valentine's day.
+                        after deciding on a direction for the project: "craigslist, but in the medieval period", we set off to
+                        create it. the site features a functioning back-end, plenty of crowd-sourced user content,
+                        and plenty of categories to refine one's search.
                         <br/>
                         <div className="projectContainer">
-                            <SocialImage />
+                            <SerfsImg />
                         </div>
                         <br/>
-                        it was created using javaScript, .
+                        it was created using javaScript, react, node, express, and mongo.
                     </p>
                     </div>
                 </div>
---
+++ src/components/serfs-list.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import serfsImage from "../images/serfs_list_1.png"
+
+const SerfsImg = () => {
+    return <img alt="serfs list main page displayed" className="projectImg" src={serfsImage} />
+}
+
+export default SerfsImg
