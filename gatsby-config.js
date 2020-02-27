f7755e518fde198660ea411c5c5c2c5573b4e0c4
nickubed <nhpquandt@gmail.com>
2020-02-27 12:38:35 -0800

    added favicon, some styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,16 +19,13 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `N_Quandt Portfolio`,
-        short_name: `starter`,
+        short_name: `nq_folio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FFC176`,
+        theme_color: `#FFC176`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/portfolio_icon.jpg`, // This path is relative to the root of the site.
       },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    }
   ],
 }
