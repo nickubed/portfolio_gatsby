9e58fdf34ae4b188434b9c61da407f13d0fb667c
nickubed <nhpquandt@gmail.com>
2020-03-02 09:56:43 -0800

    resume added and linked

--- src/components/linkIcons.jsx
+++ src/components/linkIcons.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import github from '../images/github_logo.png'
 import linkedIn from '../images/linkedIn_logo.png'
+import resume from '../resume_march_2020.pdf'
 
 const Links = () => {
     return (
         <div id="linkPack">
             <a href="https://github.com/nickubed"><img src={github} alt="github logo" id="github_logo" /></a>
             <a href="https://www.linkedin.com/in/nick-quandt/"><img src={linkedIn} alt="linkedin logo" id="linkedin_logo" /></a>
-            <p id="resume">resume</p>
+            <a href={resume}><p id="resume">resume</p></a>
         </div>
     )
 }
