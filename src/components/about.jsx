a43d7ebc64e07994f191482d9126ae1f3fb8cd18
nickubed <nhpquandt@gmail.com>
2020-03-30 11:33:48 -0700

    One section functions on large display, moving on to next sections

---
+++ src/components/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+const About = () => {
+    return (
+        <div className="container">
+            <div id="aboutContainer">
+                <h2 className="subhead">I am</h2>
+                <br/>
+                <div id="about"><p id="aboutContent">a software engineer and ex-fabricator with a passion for collaborating
+                with clients and designers, and writing scalable solutions in JavaScript, TypeScript, and snakesssssssssss.</p></div>
+            </div>
+        </div>
+    )
+}
+
+export default About
